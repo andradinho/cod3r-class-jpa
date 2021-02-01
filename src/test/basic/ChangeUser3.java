b60828ec5d22dfc59d5c4b63a48f91afdd68afe2
Andradinho <raphaelandrade2013@gmail.com>
2021-02-01 00:04:44 -0300

    Understanding the detach function.

---
+++ src/test/basic/ChangeUser3.java
@@ -0,0 +1,30 @@
+package test.basic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.basic.User;
+
+public class ChangeUser3 {
+
+public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		User user = em.find(User.class, 8L);
+		em.detach(user);
+
+		user.setName("Donatello Turtle");
+		em.merge(user); 
+		
+		em.getTransaction().commit();
+		
+		em.close();
+		emf.close();
+	}
+}
